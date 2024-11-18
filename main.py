79d05623c5293d429f64018f82ef388ab9043d08
AntoScher <asc8er@gmail.com>
2024-11-18 18:21:54 +0300

    bs4

--- main.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-img="https://img.freepik.com/free-photo/embroidery-art-handicraft_469670-35.jpg?t=st=1731770492~exp=1731774092~hmac=abb84d0051ea4aec835b7af7baf357a746f9203910a7d5dea87f298f9322688a&w=360"
-response = requests.get(img)
-with open("test.jpg", "wb") as file:
-    file.write(response.content)
