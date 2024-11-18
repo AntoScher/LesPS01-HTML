108f3c8eca922eb907fecc8f3ec73293e07040e0
AntoScher <asc8er@gmail.com>
2024-11-18 21:56:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
-def get_english_words():
-    url="https://randomword.com/"
-    try:
-        response=requests.get(url)
-        soup=BeautifulSoup(response.content, "html.parser")
-        english_words=soup.find("div",id="random_word").text.strip()
-        word_definition=soup.find("div",id="random_word_definition").text.strip()
 
-        return{
+def get_english_words():
+    url = "https://randomword.com/"
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        english_words = soup.find("div", id="random_word").text.strip()
+        word_definition = soup.find("div", id="random_word_definition").text.strip()
+
+        return {
             "english_words": english_words,
             "word_definition": word_definition
         }
-    except:
-        print("Произошла ошибка при получении данных с сайта.")
-       # return None
+    except Exception as e:
+        print("Произошла ошибка при получении данных с сайта:", e)
+        return None
 
 def word_game():
     print("Игра начинается!")
     while True:
-        word_dict=get_english_words()
-        word=word_dict.get("english_words")
-        word_definition=word_dict.get("word_definition")
+        word_dict = get_english_words()
+        if not word_dict:
+            print("Произошла ошибка при получении данных с сайта.")
+            break
+        word = word_dict.get("english_words")
+        word_definition = word_dict.get("word_definition")
         print(f"Значение слова: {word_definition}")
 
-        user=input("Что это за слово?")
-        if user==word:
+        user = input("Что это за слово? ")
+        if user.lower() == word.lower():
             print("Ответ верный!")
         else:
-            print("Ответ неверный, правльный ответ -{word}!")
-        play_again=input("хотите продолжить? y/n:")
-        if play_again.lower()!="y":
+            print(f"Ответ неверный, правильный ответ - {word}!")
+        play_again = input("Хотите продолжить? y/n: ")
+        if play_again.lower() != "y":
             print("Спасибо за игру!")
             break
 
+if __name__ == "__main__":
     word_game()
-        
